36b2257092f9857aebf9a1ab0995f3bdb40b4867
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-20 13:07:28 +0300

    Create Repository Manager

---
+++ Repositories/Abstract/IRepositoryManager.cs
@@ -0,0 +1,9 @@
+﻿using Repositories.Concrete.EFCore;
+
+namespace Repositories.Abstract;
+
+public interface IRepositoryManager
+{
+    IMovieRepository Movie { get; }
+    Task SaveAsync();
+}
---
+++ Repositories/Concrete/EFCore/RepositoryManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Abstract;
+using Repositories.Concrete.EFCore.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Concrete.EFCore
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly MovieContext _context;
+        private readonly IMovieRepository _movieRepository;
+
+        public RepositoryManager(MovieContext context, IMovieRepository movieRepository)    
+        {
+            _context = context;
+            _movieRepository = movieRepository;
+            
+        }
+
+        public IMovieRepository Movie => _movieRepository;
+        
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+        
+    }
+}
