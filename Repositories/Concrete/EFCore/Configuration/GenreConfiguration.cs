f22aab6bbc7669ad4a1088f25a1e210e9d48b4eb
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-20 12:52:57 +0300

    Add Seed Data

---
+++ Repositories/Concrete/EFCore/Configuration/GenreConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Models.Concrete.Entities;
+
+
+namespace Repositories.Concrete.EFCore.Configuration
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+
+            builder.HasData(new Genre
+            {
+                Id = 1,
+                Name = "Sci-Fi",
+            });
+
+            builder.HasData(new Genre
+            {
+                Id = 2,
+                Name = "Horror",
+            });
+
+            builder.HasData(new Genre
+            {
+                Id = 3,
+                Name = "Action",
+            });
+
+            builder.HasData(new Genre
+            {
+                Id = 4,
+                Name = "Adventure",
+            });
+
+            builder.HasData(new Genre
+            {
+                Id = 5,
+                Name = "Drama",
+            });
+        }
+
+    }
+}
--- Repositories/Concrete/EFCore/Configuration/MovieConfiguration.cs
+++ Repositories/Concrete/EFCore/Configuration/MovieConfiguration.cs
@@ -13,10 +13,59 @@ namespace Repositories.Concrete.EFCore.Configuration
 
             builder.Property(m => m.ReleaseDate).HasColumnType("date");
 
-           
-
             builder.ToTable(m => m.HasTrigger("trg_UpdateMovies"));
 
+
+            builder.HasData(new Movie
+            {
+                Id = 1,
+                Title = "The Thing",
+                ReleaseDate = new DateTime(1982, 6, 25),
+                DurationAsMinute = 119,
+                IsReleased = true,                
+                
+            });
+
+            builder.HasData(new Movie
+            {
+                Id = 2,
+                Title = "Citizen Kane",
+                ReleaseDate = new DateTime(1941, 9, 5),
+                DurationAsMinute = 120,
+                IsReleased = true,
+                
+            });
+
+            builder.HasData(new Movie
+            {
+                Id = 3,
+                Title = "Escape From New York",
+                ReleaseDate = new DateTime(1981, 7, 10),
+                DurationAsMinute = 99,
+                IsReleased = true,
+                
+            });
+
+            builder.HasData(new Movie
+            {
+                Id = 4,
+                Title = "Seven Samurai",
+                ReleaseDate = new DateTime(1954, 4, 26),
+                DurationAsMinute = 207,
+                IsReleased = true,
+                
+            });
+
+            builder.HasData(new Movie
+            {
+                Id = 5,
+                Title = "Dark City",
+                ReleaseDate = new DateTime(1998, 10, 16),
+                DurationAsMinute = 100,
+                IsReleased = true,
+                
+            });
+            
         }
     }
 }
