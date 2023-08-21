408ea02e2fad9f6cdfe2814014a7a53126eb553a
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-21 23:35:41 +0300

    Add Mapping Profile

---
+++ WebApi/Utilities/AutoMapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Models.Concrete.Entities;
+using Models.Concrete.ResponseModels.Movie;
+
+namespace WebApi.Utilities.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieResponse>();
+            CreateMap<Movie, MoviesWithAwardsResponse>();
+        }
+    }
+}
