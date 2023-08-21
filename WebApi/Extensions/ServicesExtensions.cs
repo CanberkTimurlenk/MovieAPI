059bcaf34b4737ffa1d824318ee2a596adba6bca
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-21 23:36:22 +0300

    Add ServicesExtensions

---
+++ WebApi/Extensions/ServicesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Repositories.Concrete.EFCore.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Services.Concrete;
+using Services.Abstract;
+using Repositories.Abstract;
+using Repositories.Concrete.EFCore;
+
+namespace WebApi.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+
+            => services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureServiceManager(this IServiceCollection services)
+
+            => services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services)
+
+            => services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static void RegisterRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IMovieRepository, MovieRepository>();
+
+        }
+
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IMovieService, MovieManager>();
+
+        }
+
+    }
+
+
+
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.AssemblyReference;
+using System.Reflection;
+using WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddApplicationPart(typeof(AssemblyReference).Assembly);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.RegisterRepositories();
+builder.Services.RegisterServices();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
