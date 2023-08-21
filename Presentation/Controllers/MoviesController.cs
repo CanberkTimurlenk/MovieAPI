a74244a0f2e92df6483af9b7f37ae9b3f4fc9456
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-21 23:37:24 +0300

    Create MoviesController

---
+++ Presentation/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Models.Concrete.RequestFeatures;
+using System.Text.Json;
+using Services.Abstract;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IServiceManager _serviceManager;
+        public MoviesController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet(Name = "GetMoviesByTitle")]
+        public async Task<IActionResult> GetMoviesByTitleAsync([FromQuery] string title)
+        {
+            var result = await _serviceManager.MovieService.GetMoviesByTitleAsync(title, new MovieParameters() { PageSize = 1 });
+
+            Response.Headers.Add("X-Pagination",JsonSerializer.Serialize(result.metaData));
+
+            return Ok(result.movies);
+
+            
+        }
+
+    }
+}
