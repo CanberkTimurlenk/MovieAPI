e949fb9de3f30fb86c1b1310c5dda9d73cf4f76c
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-08-21 23:32:30 +0300

    Add ServiceManager

---
+++ Services/Abstract/IServiceManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Services.Abstract
+{
+    public interface IServiceManager
+    {
+        public IMovieService MovieService { get; }
+    }
+}
---
+++ Services/Concrete/ServiceManager.cs
@@ -0,0 +1,22 @@
+﻿using Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Concrete
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly IMovieService _movieService;
+
+        public ServiceManager(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        public IMovieService MovieService => _movieService;
+
+    }
+}
