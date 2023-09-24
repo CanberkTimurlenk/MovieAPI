a6ee90d435b47c0e8885869669890abd3dd2172a
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-09-24 21:16:39 +0300

    Modify Context

--- Repositories/Concrete/EFCore/Contexts/MovieContext.cs
+++ Repositories/Concrete/EFCore/Contexts/MovieContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 using Models.Concrete.Entities;
+using Models.Concrete.Entities.Junctions;
+using Models.Concrete.Domains.Junctions;
 
 namespace Repositories.Concrete.EFCore.Contexts
 {
@@ -10,22 +12,27 @@ namespace Repositories.Concrete.EFCore.Contexts
         {
 
         }
-
+      
+        
         public DbSet<AwardType> AwardTypes { get; set; }
         public DbSet<Award> Awards { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<PersonGenre> PersonGenres { get; set; }
         public DbSet<Language> Languages { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Movie> Movies { get; set; }
-        public DbSet<MovieDetail> MovieDetails { get; set; }
-        public DbSet<MovieRole> MovieRoles { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+        public DbSet<MovieLanguage> MovieLanguages { get; set; }
+        public DbSet<MovieLocation> MovieLocations { get; set; }
+        public DbSet<MovieDetail> MovieDetails { get; set; }        
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Director> Directors { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer(@"");
+            
         }
 
 
