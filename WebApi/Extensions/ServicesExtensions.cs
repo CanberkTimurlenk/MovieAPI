10db45613d32903e817f3f07359409402a92b05f
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-09-24 21:24:35 +0300

    Add DI LifeTimes

--- WebApi/Extensions/ServicesExtensions.cs
+++ WebApi/Extensions/ServicesExtensions.cs
@@ -26,12 +26,30 @@ namespace WebApi.Extensions
         public static void RegisterRepositories(this IServiceCollection services)
         {
             services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+            services.AddScoped<IAwardRepository, AwardRepository>();
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            services.AddScoped<ILocationRepository, LocationRepository>();
+            services.AddScoped<IAwardTypeRepository, AwardTypeRepository>();
+            services.AddScoped<ILanguageRepository, LanguageRepository>();
+            services.AddScoped<IMovieDetailRepository, MovieDetailRepository>();
+            services.AddScoped<IActorRepository, ActorRepository>();
+            services.AddScoped<IDirectorRepository, DirectorRepository>();
 
         }
 
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddScoped<IMovieService, MovieManager>();
+            services.AddScoped<IGenreService, GenreManager>();
+            services.AddScoped<IAwardService, AwardManager>();
+            services.AddScoped<IPersonService, PersonManager>();
+            services.AddScoped<ILocationService, LocationManager>();
+            services.AddScoped<IAwardTypeService, AwardTypeManager>();
+            services.AddScoped<ILanguageService, LanguageManager>();
+            services.AddScoped<IMovieDetailService, MovieDetailManager>();
+            services.AddScoped<IActorService, ActorManager>();
+            services.AddScoped<IDirectorService, DirectorManager>();
 
         }
 
