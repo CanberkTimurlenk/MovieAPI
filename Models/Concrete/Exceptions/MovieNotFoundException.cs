4a9f819536cc2cce5dc9acb17718b2ced724783e
CanberkTimurlenk <canberk.timurlenk@gmail.com>
2023-09-25 23:00:12 +0300

    Add Custom Exception Classes

---
+++ Models/Concrete/Exceptions/MovieNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Concrete.Exceptions
+{
+    public sealed class MovieNotFoundException : NotFoundException
+    {
+        public MovieNotFoundException(int id) : base($"The Movie with id : {id} could not found.")
+        {
+
+        }
+    }
+}
