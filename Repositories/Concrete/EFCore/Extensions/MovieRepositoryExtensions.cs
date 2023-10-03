46bc1141db4c51bc5f49fca6ab7bf8a006edb916
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-03 22:34:23 +0300

    Add Extensions for Sort By Query

---
+++ Repositories/Concrete/EFCore/Extensions/MovieRepositoryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Models.Concrete.Entities;
+using System.Linq.Dynamic.Core;
+
+namespace Repositories.Concrete.EFCore.Extensions
+{
+    public static class MovieRepositoryExtensions
+    {
+        public static IQueryable<Movie> Sort(this IQueryable<Movie> movie, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return movie;
+
+            var orderQuery = OrderQueryBuilder<Movie>.CreateQueryString(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return movie.OrderBy(m => m.Id);
+
+            return movie.OrderBy(orderQuery);
+
+        }
+    }
+}
---
+++ Repositories/Concrete/EFCore/Extensions/OrderQueryBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Models.Abstract.Entities;
+using System.Reflection;
+using System.Text;
+
+namespace Repositories.Concrete.EFCore.Extensions
+{
+    public class OrderQueryBuilder<T>
+        where T : class, IEntity, new()
+
+    {
+        public static string CreateQueryString(string orderByQueryString)
+
+        {
+            var type = typeof(T);
+            var orderParams = orderByQueryString.Trim().Split(", ").ToList();
+            var propertyInfos = type.GetProperties(BindingFlags.Instance | BindingFlags.Public).ToList();
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+
+                var matchingPropertyName = propertyInfos.FirstOrDefault(p => p.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase)).Name;
+
+                if (matchingPropertyName is null)
+                    continue;
+
+                var direction = param.EndsWith("desc") ? "desc" : "asc";
+
+                orderQueryBuilder.Append($"{matchingPropertyName} {direction},");
+
+            }
+
+            return orderQueryBuilder.ToString().TrimEnd(' ', ',');
+        }
+    }
+}
+
