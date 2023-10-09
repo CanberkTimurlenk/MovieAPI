ae9183ef882cc5e03a8e23d6ce179d5d7972b64a
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-09 23:10:01 +0300

    Add Redis Cache

---
+++ Services/CrossCuttingConcerns/Caching/Abstract/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.CrossCuttingConcerns.Caching.Abstract
+{
+    public interface ICacheService
+    {
+        T Get<T>(string cacheKey);
+        void Add(string key, object data, TimeSpan cacheDuration);
+        Task<IEnumerable<object>> Get(string key, Type type);
+    }
+}
---
+++ Services/CrossCuttingConcerns/Caching/Concrete/Redis/RedisCacheManager.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Services.CrossCuttingConcerns.Caching.Abstract;
+using System.Text;
+using System.Text.Json;
+
+namespace Services.CrossCuttingConcerns.Caching.Concrete.Redis
+{
+    public class RedisCacheManager : ICacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public RedisCacheManager(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public T Get<T>(string key)
+        {
+            var jsonData = _cache.GetString(key);
+
+            if (jsonData is null)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+
+        }
+
+        public async Task<object> Get(string key, Type type)
+        {
+            var jsonData = await _cache.GetStringAsync(key);
+
+            if (jsonData is null)
+                return default;
+
+            return JsonSerializer.Deserialize(jsonData, type);
+        }
+
+        public void Add(string key, object data, TimeSpan cacheDuration)
+        {
+
+
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = cacheDuration
+            };
+
+            var jsonData = JsonSerializer.Serialize(data);
+            _cache.SetString(key, jsonData, options);
+
+        }
+    }
+}
