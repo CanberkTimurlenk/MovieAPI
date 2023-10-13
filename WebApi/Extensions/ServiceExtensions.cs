6c0eb6b37b8ba13cf5b2017a91e386e66d33c103
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-13 15:06:26 +0300

    Modify Service Extensions For Autofac

---
+++ WebApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Repositories.Concrete.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Services.Abstract;
+using Repositories.Abstract;
+using Repositories.Concrete.EFCore;
+using Services.Concrete;
+
+namespace WebApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+
+            => services.AddDbContext<MovieContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureRedis(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = configuration.GetConnectionString("redisConn");
+                options.InstanceName = "MovieCatalog";
+            });
+        }
+    }
+}
