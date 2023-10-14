d8764c41ee1bc3e9433dbff3290ef262d7cd2b43
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-14 22:31:21 +0300

    Add Service Collection Extensions

---
+++ Services/DependencyResolvers/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services.Utilities.Ioc;
+using Services.DependencyResolvers.Autofac.CoreModule;
+
+namespace Services.DependencyResolvers.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDependencyResolvers(this IServiceCollection serviceCollection, IAspectModule[] modules)
+        {
+            foreach (var module in modules)
+                module.Load(serviceCollection);
+
+            return ServiceTool.Create(serviceCollection);
+        }
+    }
+}
