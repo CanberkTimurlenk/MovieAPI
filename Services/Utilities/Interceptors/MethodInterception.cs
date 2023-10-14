3981dbe7d1c5fda31142b714442296748b34b75b
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-14 22:28:26 +0300

    Add Interceptors

---
+++ Services/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,37 @@
+﻿using Castle.DynamicProxy;
+
+namespace Services.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, Exception ex) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                isSuccess = false;
+                OnException(invocation, ex);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                    OnSuccess(invocation);
+            }
+
+            OnAfter(invocation);
+        }
+    }
+}
