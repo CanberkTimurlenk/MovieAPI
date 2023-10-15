4466acdb92bece696d887016d24482447a2aefe1
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-15 19:31:49 +0300

    Add Entity / User

---
+++ Models/Concrete/Domain/User.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System.Diagnostics;
+
+namespace Models.Concrete.Domain
+{
+    public class User : IdentityUser
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
