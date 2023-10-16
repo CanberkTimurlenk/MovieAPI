8287860d4e75c8168024a85d3c83fe82af23017e
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-16 22:57:09 +0300

    Add UserForRegisterRequest

---
+++ Models/Concrete/RequestModels/Insertion/User/UserForRegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Concrete.RequestModels.Insertion.User
+{
+    public record UserForRegisterRequest
+    {
+        public string? Firstname { get; init; }
+        public string? Lastname { get; init; }
+        public string Username { get; init; }
+        public string Password { get; init; }
+        public string Email { get; init; }
+        public string? PhoneNumber { get; init; }
+
+    }
+}
