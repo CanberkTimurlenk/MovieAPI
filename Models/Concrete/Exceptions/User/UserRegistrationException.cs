591c2726238294068add2326e2de0bbbd028fc0e
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-21 21:15:01 +0300

    Add Exceptions for User

---
+++ Models/Concrete/Exceptions/User/UserRegistrationException.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.Concrete.Exceptions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Concrete.Exceptions.User
+{
+    public sealed class UserRegistrationException : BadRequestException
+    {
+        public IEnumerable<IdentityError> Errors { get; private set; }
+        public UserRegistrationException() : base("Registration was not successful") { }
+        public UserRegistrationException(string message) : base(message) { }
+
+        public UserRegistrationException(IEnumerable<IdentityError> errors)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var item in errors)
+                builder.Append(item.Description);
+
+            throw new UserRegistrationException(builder.ToString().TrimEnd());
+        }
+
+    }
+}
---
+++ Models/Concrete/Exceptions/User/WrongPasswordException.cs
@@ -0,0 +1,17 @@
+﻿using Models.Concrete.Exceptions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Concrete.Exceptions.User
+{
+    public sealed class InvalidCredentialsException : UnauthorizedException
+    {
+        public InvalidCredentialsException() : base($"Wrong password or invalid credentials.") { }
+
+        public InvalidCredentialsException(string message) : base(message) { }
+
+    }
+}
