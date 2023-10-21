239cba53fbdaf1906c4a8ee9665387213cc5646d
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-21 21:14:24 +0300

    Refactor Not Found Exception

--- Models/Concrete/Exceptions/User/UserNotFoundException.cs
+++ Models/Concrete/Exceptions/User/UserNotFoundException.cs
@@ -7,11 +7,9 @@ using System.Threading.Tasks;
 
 namespace Models.Concrete.Exceptions.User
 {
-    internal class UserNotFoundException : NotFoundException
+    public sealed class UserNotFoundException : NotFoundException
     {
-        public UserNotFoundException(string email) : base($"The User with id : {email} could not found.")
-        {
+        public UserNotFoundException(string username) : base($"Username : {username} could not found.") { }
 
-        }
     }
 }
