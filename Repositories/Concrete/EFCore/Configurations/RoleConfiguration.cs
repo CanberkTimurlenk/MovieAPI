66fbaf4a720d006ef8ca98c6b0d57c276b2efde1
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-21 21:16:31 +0300

    Add Roles (IdentityRole)

---
+++ Repositories/Concrete/EFCore/Configurations/RoleConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Concrete.EFCore.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+
+                });
+
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+
+                });
+        }
+    }
+}
