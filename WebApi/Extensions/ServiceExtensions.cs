3ff73db6adbfd51d76eefd395c0df050173da408
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 18:50:15 +0300

    Refactor Service Extensions

--- WebApi/Extensions/ServiceExtensions.cs
+++ WebApi/Extensions/ServiceExtensions.cs
@@ -1,5 +1,12 @@
 ﻿using Repositories.Concrete.EFCore.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Models.Concrete.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AspNetCoreRateLimit;
+using AspNetCoreRateLimit.Redis;
 using Services.DependencyResolvers.Autofac.CoreModule;
 using Services.DependencyResolvers.Extensions;
 
@@ -19,6 +26,69 @@ namespace WebApi.Extensions
                 options.Configuration = configuration.GetConnectionString("redisConn");
                 options.InstanceName = "MovieCatalog";
             });
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            var builder = services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+
+                options.User.RequireUniqueEmail = true;
+            })
+                .AddEntityFrameworkStores<MovieContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["secretKey"];
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["validIssuer"],
+                    ValidAudience = jwtSettings["validAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+        }
+
+        public static void ConfigureRateLimitingOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rateLimitRules = new List<RateLimitRule>()
+            {
+                new RateLimitRule()
+                {
+                    Endpoint = "*",
+                    Limit = 100,
+                    Period = "1m"
+                }
+            };
+
+            services.Configure<IpRateLimitOptions>(opt =>
+            {
+                opt.GeneralRules = rateLimitRules;
+            });
+
+            services.AddDistributedRateLimiting<AsyncKeyLockProcessingStrategy>();
+            services.AddDistributedRateLimiting<RedisProcessingStrategy>();
+            services.AddRedisRateLimiting();
+
         }
 
         public static void ConfigureAspects(this IServiceCollection services)
