c2a5a4e5aa992d2dd513ecd739d4af8af756f942
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 18:37:10 +0300

    Add Log Aspect

---
+++ Services/Aspects/Logging/LogAspect.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using Serilog;
+using Services.Utilities.Interceptors;
+
+namespace Services.Aspects.Logging
+{
+    public class LogAspect : MethodInterception
+    {
+        protected override void OnException(IInvocation invocation, Exception ex)
+        {
+            Log.Error("An exception was thrown : {@Message} , {@Exception}", ex.Message, ex);
+
+            Log.CloseAndFlush();
+        }
+    }
+}
