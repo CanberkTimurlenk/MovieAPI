21e11dd906df6d12d3d2ede6ab279e763d2ffc9c
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 17:18:39 +0300

    Add Unauthorized Exception

---
+++ Models/Concrete/Exceptions/Common/UnauthorizedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Concrete.Exceptions.Common
+{
+    public abstract class UnauthorizedException : Exception
+    {
+        protected UnauthorizedException(string message) : base(message) { }
+
+    }
+}
