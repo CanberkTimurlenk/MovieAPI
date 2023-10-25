6c3f9f1c4d4eb577f39f275de34aee10607daadf
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 18:24:52 +0300

    Register Redis Stored Rate Limiting

---
+++ WebApi/DependencyResolvers/Autofac/ApiModule.cs
@@ -0,0 +1,21 @@
+﻿using AspNetCoreRateLimit;
+using Autofac;
+using StackExchange.Redis;
+
+namespace WebApi.DependencyResolvers.Autofac
+{
+    public class ApiModule : Module
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DistributedCacheIpPolicyStore>().As<IIpPolicyStore>().SingleInstance();
+            builder.RegisterType<DistributedCacheRateLimitCounterStore>().As<IRateLimitCounterStore>().SingleInstance();
+            builder.RegisterType<RateLimitConfiguration>().As<IRateLimitConfiguration>().SingleInstance();
+
+            var redisOptions = ConfigurationOptions.Parse(Configuration["ConnectionStrings:redisConn"]);
+            builder.Register(provider => ConnectionMultiplexer.Connect(redisOptions)).As<IConnectionMultiplexer>().SingleInstance();
+        }
+    }
+}
