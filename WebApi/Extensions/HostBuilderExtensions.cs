30e8313131cfc48e7725a5efd398cbf9f7e3e093
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 18:29:56 +0300

    Add RateLimiting to HostBuilder Extensios

--- WebApi/Extensions/HostBuilderExtensions.cs
+++ WebApi/Extensions/HostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac.Extensions.DependencyInjection;
 using Autofac;
 using Services.DependencyResolvers.Autofac;
+using WebApi.DependencyResolvers.Autofac;
 
 namespace WebApi.Extensions
 {
@@ -8,10 +9,12 @@ namespace WebApi.Extensions
     {
         public static void ConfigureAutofac(this ConfigureHostBuilder host, IConfiguration configuration)
         {
+            ApiModule.Configuration = configuration;
             host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
             host.ConfigureContainer<ContainerBuilder>(builder =>
             {
                 builder.RegisterModule(new ServiceModule());
+                builder.RegisterModule(new ApiModule());
 
             });
         }
