a93bda68e3eaed78cd22444eafa467a2c5d08f6a
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-10-25 18:12:41 +0300

    Refactor Autofac

---
+++ WebApi/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Services.DependencyResolvers.Autofac;
+
+namespace WebApi.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        public static void ConfigureAutofac(this ConfigureHostBuilder host, IConfiguration configuration)
+        {
+            host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            host.ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new ServiceModule());
+
+            });
+        }
+    }
+}
